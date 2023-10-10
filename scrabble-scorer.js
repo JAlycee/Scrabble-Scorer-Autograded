83566748b59d3a21378113fc7498406beb6c6974
JAlycee <124473879+JAlycee@users.noreply.github.com>
2023-10-09 22:11:31 -0500

    GRaded Assignment

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -29,22 +29,84 @@ function oldScrabbleScorer(word) {
 	return letterPoints;
  }
 
+function passingOldScorer(scoreFunction, word) {
+let score = scoreFunction(word);
+console.log(score);
+}
+
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   let word = input.question("Let's play some scrabble! Enter a word: ");
+
+   word = word.toLowerCase();
+
+   let result = oldScrabbleScorer(word);
+
+   console.log(`Points for '${result}':`);
 };
 
-let simpleScorer;
+function simpleScorer(word) {
+   word = word.toLowerCase();
+   let totalScore = 0;
+   for (let i = 0; i <word.length; i++) {
+      totalScore += 1;
+   }
+   return totalScore;
+}
 
-let vowelBonusScorer;
+function vowelBonusScorer(word) {
+word = word.toLowerCase();
+
+let totalScore = 0;
+
+const vowels = ['a', 'e', 'i','o','u'];
+
+for (let i = 0; i <word.length; i++) {
+   if (vowels.has(word[i])) {
+      totalScore += 3;
+   } else {
+      totalScore += 1;
+   }
+}
+return totalScore;
+}
 
 let scrabbleScorer;
 
-const scoringAlgorithms = [];
+const scoringAlgorithms = [
+   {
+      name: "Simple Score",
+      description: "Each letter is worth 1 point.",
+      scoringFunction: simpleScorer,
+      },
+      {
+         name: "Bonus Vowels",
+         description: "Vowels are 3 pts, consonants are 1 pt.",
+         scoringFunction: vowelBonusScorer,
+      },
+      {
+         name: "Scrabble",
+         description: "The traditional Scrabble scoring method.", 
+         scoringFunction: oldScrabbleScorer,
+      }
+];
 
-function scorerPrompt() {}
+function scorerPrompt() {
+   console.log("Scoring Options:");
+   for (let i = 0; i <scoringAlgorithms.length; i++) {
+      let algorithm = scoringAlgorithms[i];
+      console.log(`${i} ${algorithm.name}: ${algorithm.description}`);
+   }
+}
+let selectedOption = input.question("Select a scoring method 0, 1, 2.");
+if (selectedOption>= 0 && selectedOption <= 2) {
+   return scoringAlgorithms[selectedOption];
+} else {
+   console.log("Invalid input.Please enter a number between 0 and 2.");
+   return totalScore;
+}
 
 function transform() {};
 
